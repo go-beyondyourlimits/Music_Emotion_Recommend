f42d88b8d2613c341118580517400781889ad28f
RyosukeOzaki <ryoryoryor102510@gmail.com>
2021-09-04 20:43:37 +0900

    Add run.sh for docker

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# それぞれのサーバーの起動
+docker-compose run --rm web-back sh -c "python manage.py makemigrations"
+docker-compose run --rm web-back sh -c "python manage.py migrate"
+docker-compose run --rm web-back sh -c "python manage.py createsuperuser"
+docker-compose run --rm web-front sh -c "yarn add next react"
